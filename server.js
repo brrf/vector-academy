277ec141cc980a81de34a8484c22a8b9035a0474
Moshe Praver <moshepraver@gmail.com>
2020-01-04 19:41:36 -0600

    trying secondary route

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use ((req, res, next) => {
 });
 
 app.use(express.static('public'));
+app.use(express.static('css'));
 
 // app.get('/', (req, res)=> {
 //   res.sendFile(path.join(__dirname, 'public', 'index.html'));
