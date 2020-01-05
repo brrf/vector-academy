4081ec9d008f99fcaa0fb1064a30fd3ee860dd89
Moshe Praver <moshepraver@gmail.com>
2020-01-04 19:24:53 -0600

    removed / route

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ app.use ((req, res, next) => {
 
 app.use(express.static('public'));
 
-app.get('/', (req, res)=> {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
+// app.get('/', (req, res)=> {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// });
 
 //404 Not Found Middleware
 app.use(function(req, res, next) {
