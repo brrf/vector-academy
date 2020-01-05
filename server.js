44e11d23d427f37f9017ca7b800d73095812ff3c
Moshe Praver <moshepraver@gmail.com>
2020-01-04 19:21:17 -0600

    changed server static folder

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use ((req, res, next) => {
   next()
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('public'));
 
 app.get('/', (req, res)=> {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
