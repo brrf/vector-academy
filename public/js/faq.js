8c715bdaa56d453b0455f15cad31490575e2aec7
Moshe Praver <moshepraver@gmail.com>
2020-01-12 15:26:55 -0600

    finished FAQ section content and fixed js bugs

--- public/js/faq.js
+++ public/js/faq.js
@@ -4,9 +4,11 @@ let accordion = document.querySelector('#faq-section');
 let accordionItems = document.querySelectorAll('.accordion-item');
 
 accordion.addEventListener('click', function(event) {
-	const li = event.target.closest('li');
-	if (!li) return;
-	const isAlreadyAcive = li.classList.contains('is-active') ? true : false
+	const accordionItem = event.target.closest('.accordion-item');
+	const accordionPanel = event.target.closest('.accordion-panel') || event.target.nextElementSibling;
+
+	if (!accordionItem) return;
+	const isAlreadyAcive = accordionItem.classList.contains('is-active') ? true : false
 
 	for (let item of accordionItems) {
 		item.classList.remove('is-active');
@@ -14,11 +16,10 @@ accordion.addEventListener('click', function(event) {
 	}
 	
 	if (isAlreadyAcive) {
-		li.classList.remove('is-active');
-		li.lastElementChild.classList.remove('display');
+		accordionItem.classList.remove('is-active');
+		accordionPanel.classList.remove('display');
 	} else {
-		li.classList.add('is-active');
-		li.lastElementChild.classList.add('display');
+		accordionItem.classList.add('is-active');		
+		accordionPanel.classList.add('display');
 	}
-	
-})
+});
