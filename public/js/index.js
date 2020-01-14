7da35130cbe1c1b06e99d5557461c9a7d20ffc98
Moshe Praver <moshepraver@gmail.com>
2020-01-14 16:22:39 -0600

    updated supported browsers

--- public/js/index.js
+++ public/js/index.js
@@ -3,19 +3,19 @@
 //template the header, footer and main-inactive sections
 let header = document.createElement('header');
 header.id = 'navbar-container';
-header.innerHTML = "<h1><a href=\"/\">Vector</a></h1><div class='navbar-large'><ul class='navbar-links'><li>About &#9662<ul class='dropdown hidden'><li><a href=\"/howitworks\">How it works</a></li><li><a href=\"/prereqs\">Prerequisites</a></li><li><a href=\"./faqs\">FAQs</a></li></ul></li><li><a href=\"./employerlogin\">Employer Login</a></li><li><a href=\"./employerlogin\">Student Login</a></li><li><button>Apply</button></li></ul></div><div class='navbar-mobile'><button class=\"hamburger hamburger--spin\" type=\"button\"><span class=\"hamburger-inner\"></span></button></div>";
+header.innerHTML = `<h1><a href="/">Vector</a></h1><div class='navbar-large'><ul class='navbar-links'><li>About &#9662<ul class='dropdown hidden'><li><a href="/howitworks">How it works</a></li><li><a href="/prereqs">Prerequisites</a></li><li><a href="./faqs">FAQs</a></li></ul></li><li><a href="./employerlogin">Employer Login</a></li><li><a href="./employerlogin">Student Login</a></li><li><button>Apply</button></li></ul></div><div class='navbar-mobile'><button class="hamburger hamburger--spin" type="button"><span class="hamburger-inner"></span></button></div>`;
 document.body.prepend(header);
 let footer = document.createElement('footer');
-footer.innerHTML = "<div class='footer-container'><h1><a href=\"/\">Vector</a></h1><div><h3>About</h3><ul><li><a href=\"./privacy\">Privacy Policy</a></li><li><a href=\"./privacypolicy\">Contact</a></li></ul></div><div><h3>Resources</h3><ul><li><a href=\"./faqs\">FAQ</a></li><li><a href=\"./privacypolicy\">How it works</a></li><li><a href=\"./privacypolicy\">Application</a></li></ul></div></div><p></p>";
+footer.innerHTML = `<div class='footer-container'><h1><a href="/">Vector</a></h1><div><h3>About</h3><ul><li><a href="./privacy">Privacy Policy</a></li><li><a href="./privacypolicy">Contact</a></li></ul></div><div><h3>Resources</h3><ul><li><a href="./faqs">FAQ</a></li><li><a href="./privacypolicy">How it works</a></li><li><a href="./privacypolicy">Application</a></li></ul></div></div><p></p>`;
 document.body.append(footer);
 let mainInactive = document.createElement('main');
 mainInactive.className = 'main-inactive hidden';
-mainInactive.innerHTML = "<a href=\"/howitworks\"><h3>How it works</h3></a><a href=\"prereqs\"><h3>Prerequisites</h3></a><a href=\"./faqs\"><h3>FAQs</h3></a><hr><a href=\"./employerlogin\"><h3>Employer Login</h3></a><a href=\"./employerlogin\"><h3>Student Login</h3></a><a href=\"'./apply\"><button>Apply</button></a>";
+mainInactive.innerHTML = `<a href="/howitworks"><h3>How it works</h3></a><a href="prereqs"><h3>Prerequisites</h3></a><a href="./faqs"><h3>FAQs</h3></a><hr><a href="./employerlogin"><h3>Employer Login</h3></a><a href="./employerlogin"><h3>Student Login</h3></a><a href="'./apply"><button>Apply</button></a>`;
 footer.before(mainInactive); //update copyright by year
 
 let copyright = document.querySelector('footer p');
 const year = new Date().getFullYear();
-copyright.innerHTML = "Copyright &copy; ".concat(year, " Vector Training Academy. All Rights Reserved."); //toggle active of hamburger
+copyright.innerHTML = `Copyright &copy; ${year} Vector Training Academy. All Rights Reserved.`; //toggle active of hamburger
 
 let hamburger = document.querySelector('.hamburger');
 let mainActive = document.querySelector('.main-active');
