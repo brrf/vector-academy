9f226e19d7ebb9b9e09f17aed63819f124e6185e
Moshe Praver <moshepraver@gmail.com>
2020-03-25 17:16:12 -0500

    updated production server

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const db = require('./db');
 const cors = require('cors');
 require('dotenv').config();
 
-const server = require('./server.common.js');
+const mainAppServer = require('./main-app-server.js');
+const marketingAppServer = require('./marketing-app-server.js');
 
 const marketingApp = express();
 const mainApp = express();
@@ -19,30 +20,49 @@ marketingApp.use ((req, res, next) => {
   next()
 });
 
-//Virtual Routing Application
-const commonApp = express();
 
-commonApp.use(cors({
+marketingApp.use(cors({
   credentials: true, 
   origin: ['https://localhost:3001', 'https://www.vectoracademy.io']
 }));
 
-commonApp.use(vhost('localhost', marketingApp));
-commonApp.use(vhost('apply.localhost', mainApp));
+mainApp.use ((req, res, next) => {
+  res.header ('Access-Control-Allow-Origin', 'https://apply.vectoracademy.io')
+  res.header ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept')
+  res.header ('Access-Control-Allow-Credentials', true)
+  res.header ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+  next()
+});
 
-server(marketingApp, mainApp, commonApp, 'dist');
+mainApp.use(cors({
+  credentials: true, 
+  origin: ['https://localhost:3002', 'https://apply.vectoracademy.io']
+}));
+
+mainAppServer(mainApp, 'dist');
+marketingAppServer(marketingApp, 'dist');
 
 
 //404 Not Found Middleware
-commonApp.use(function(req, res, next) {
+marketingApp.use(function(req, res, next) {
+  res.status(404)
+    .type('text')
+    .send('Not Found');
+});
+
+mainApp.use(function(req, res, next) {
   res.status(404)
     .type('text')
     .send('Not Found');
 });
 
 //Start server
-mainApp.listen(process.env.PORT || 3001, function () {
-  console.log("Listening on port " + process.env.PORT);
+marketingApp.listen(3001, function () {
+  console.log("Listening on port " + 3001);
+});
+
+mainApp.listen(3002, function () {
+  console.log("Listening on port " + 3002);
 });
 
 //Connect to database
