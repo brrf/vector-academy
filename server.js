f416dab4d6a864124c8d76bec5e66db316e532ba
Moshe Praver <moshepraver@gmail.com>
2020-04-30 16:56:18 -0500

    allow access cross origin

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ mainApp.use(cors({
 }));
 
 employerApp.use ((req, res, next) => {
-  res.header ('Access-Control-Allow-Origin', 'https://hire.vectoracademy.io')
+  res.header ('Access-Control-Allow-Origin', '*')
   res.header ('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept')
   res.header ('Access-Control-Allow-Credentials', true)
   res.header ('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
