ca209472da0721051338e0b251f0edfb672e7ab1
Moshe Praver <moshepraver@gmail.com>
2020-04-30 19:28:41 -0500

    updated links on marketing app

--- marketing-app/src/app/utils/template-page.js
+++ marketing-app/src/app/utils/template-page.js
@@ -8,43 +8,41 @@ export default function templatePage () {
 
 	const header = document.createElement('header');
 	header.id = 'navbar-container';
-	header.innerHTML = `<a href=\"./\"><img src=${logo} alt=\"logo\"></a><div class='navbar-large'><ul class='navbar-links'><li>About &#9662<ul class='dropdown hidden'><li><a href=\"./howitworks\">How it works</a></li><li><a href=\"./timeline\">Application Timeline</a></li><li><a href=\"./faqs\">FAQs</a></li></ul></li><li id=\"employer-login\">Employer Login</li><li id=\"student-login\"><a href=\"${PROTOCOL}apply.${DOMAIN}\">Student Login</a></li><li><button>Apply</button></li></ul></div><div class='navbar-mobile'><button class=\"hamburger hamburger--spin\" type=\"button\"><span class=\"hamburger-inner\"></span></button></div><div class="login-modal employer-modal hidden"><div id='form-container' class="login-form-container"><span>&times;</span><h2>Employer Login</h2><form><input data-type="text" class="form-full-width" name="institutionID" autocomplete="email" maxlength="50" placeholder="Institution ID" required><input type="password" autocomplete="current-password" class="form-full-width" name="password" maxlength="50" placeholder="Password" required><div class='form-submit-button-container'><button>Login</button></div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<a href="./contact"><u>Contact us!</u></a></p></div></div><div class="login-modal student-login-modal hidden"><div id='form-container' class="login-form-container student-login-form-container"><span>&times;</span><h2>Student Login</h2><form><input data-type="text" autocomplete="email" class="form-full-width" name="email" maxlength="50" placeholder="E-mail" required><input type="password" autocomplete="current-password" class="form-full-width" name="password" maxlength="50" placeholder="Password" required> 	<div class='form-submit-button-container'><button>Login</button></div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<u>Sign up here!</u></p></div></div><div class="login-modal student-register-modal hidden"><div id='form-container' class="student-register-form-container login-form-container"><span>&times;</span><h2>Register</h2><form id="register-form"><input data-type="text" type="e-mail" class="form-full-width" name="email" maxlength="50" placeholder="E-mail" required><input type="password" class="form-full-width" name="password" autocomplete="new-password" maxlength="50" placeholder="Password" required><input type="password" class="form-full-width" autocomplete="new-password" name="password2" maxlength="50" placeholder="Confirm Password" required>	<div class='form-submit-button-container'><button >Register</button></div></form><p>Already have an account?&nbsp;&nbsp;&nbsp;<u>Sign in here!</u></p></div></div>`;
+	header.innerHTML = `<a href=\"./\"><img src=${logo} alt=\"logo\"></a><div class='navbar-large'><ul class='navbar-links'><li>About &#9662<ul class='dropdown hidden'><li><a href=\"./howitworks\">How it works</a></li><li><a href=\"./timeline\">Application Timeline</a></li><li><a href=\"./faqs\">FAQs</a></li></ul></li><li id=\"employer-login\"><a href="https://hire.vectoracademy.io">Employer Login</a></li><li id=\"student-login\"><a href="https://apply.vectoracademy.io">Student Login</a></li><li><button>Apply</button></li></ul></div><div class='navbar-mobile'><button class=\"hamburger hamburger--spin\" type=\"button\"><span class=\"hamburger-inner\"></span></button></div><div class="login-modal employer-modal hidden"><div id='form-container' class="login-form-container"><span>&times;</span><h2>Employer Login</h2><form><input data-type="text" class="form-full-width" name="institutionID" autocomplete="email" maxlength="50" placeholder="Institution ID" required><input type="password" autocomplete="current-password" class="form-full-width" name="password" maxlength="50" placeholder="Password" required><div class='form-submit-button-container'><button>Login</button></div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<a href="./contact"><u>Contact us!</u></a></p></div></div><div class="login-modal student-login-modal hidden"><div id='form-container' class="login-form-container student-login-form-container"><span>&times;</span><h2>Student Login</h2><form><input data-type="text" autocomplete="email" class="form-full-width" name="email" maxlength="50" placeholder="E-mail" required><input type="password" autocomplete="current-password" class="form-full-width" name="password" maxlength="50" placeholder="Password" required> 	<div class='form-submit-button-container'><button>Login</button></div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<u>Sign up here!</u></p></div></div><div class="login-modal student-register-modal hidden"><div id='form-container' class="student-register-form-container login-form-container"><span>&times;</span><h2>Register</h2><form id="register-form"><input data-type="text" type="e-mail" class="form-full-width" name="email" maxlength="50" placeholder="E-mail" required><input type="password" class="form-full-width" name="password" autocomplete="new-password" maxlength="50" placeholder="Password" required><input type="password" class="form-full-width" autocomplete="new-password" name="password2" maxlength="50" placeholder="Confirm Password" required>	<div class='form-submit-button-container'><button >Register</button></div></form><p>Already have an account?&nbsp;&nbsp;&nbsp;<a href='https://apply.vectoracademy.io'><u>Sign in here!</u></a></p></div></div>`;
 	document.body.insertBefore(header, mainActive);
 	const mainInactive = document.createElement('main');
 	mainInactive.className = 'main-inactive hidden';
-	mainInactive.innerHTML = `<div class=\"main-inactive-menu\"><a href=\"./howitworks\"><h3>How it works</h3></a><a href=\"./timeline\"><h3>Application Timeline</h3></a><a href=\"./faqs\"><h3>FAQs</h3></a><hr><h3 id=\"employer-login-mobile\">Employer Login</h3><h3 id=\"student-login-mobile\"><a href=\"${PROTOCOL}apply.${DOMAIN}\">Student Login</a></h3><h3><button class=\"student-register-mobile\">Apply</button></h3></div><div class=\"employer-modal-mobile hidden\"><div id='form-container' class=\"login-form-container\"><h2>Employer Login</h2><form><input data-type=\"text\" autocomplete=\"email\" class=\"form-full-width\" name=\"institutionID\" maxlength=\"50\" placeholder=\"Institution ID\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required><div class='form-submit-button-container'><button>Login</button>	</div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<a href=\"./contact\"><u>Contact us!</u></a></p></div></div><div class=\"student-login-modal-mobile hidden\"><div id='form-container' class=\"login-form-container student-login-form-container\"><h2>Student Login</h2><form><input data-type=\"text\" autocomplete=\"email\" class=\"form-full-width\" name=\"email\" maxlength=\"50\" placeholder=\"E-mail\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required> 	<div class='form-submit-button-container'><button>Login</button>	</div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<u>Sign up here!</u></p></div></div><div class=\"student-register-modal-mobile hidden\"><div id='form-container' class=\"student-register-form-container login-form-container\"><h2>Register</h2><form id='register-form-mobile'><input data-type=\"text\" type=\"e-mail\" class=\"form-full-width\" name=\"email\" maxlength=\"50\" placeholder=\"E-mail\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password2\" maxlength=\"50\" placeholder=\"Confirm Password\" required><div class='form-submit-button-container'><button >Register</button></div></form><p>Already have an account?&nbsp;&nbsp;&nbsp;<u>Sign in here!</u></p></div></div>`;
+	mainInactive.innerHTML = `<div class=\"main-inactive-menu\"><a href=\"./howitworks\"><h3>How it works</h3></a><a href=\"./timeline\"><h3>Application Timeline</h3></a><a href=\"./faqs\"><h3>FAQs</h3></a><hr><h3 id=\"employer-login-mobile\"><a href="https://hire.vectoracademy.io">Employer Login</a></h3><h3 id=\"student-login-mobile\"><a href="https://apply.vectoracademy.io">Student Login</a></h3><h3><button class=\"student-register-mobile\">Apply</button></h3></div><div class=\"employer-modal-mobile hidden\"><div id='form-container' class=\"login-form-container\"><h2>Employer Login</h2><form><input data-type=\"text\" autocomplete=\"email\" class=\"form-full-width\" name=\"institutionID\" maxlength=\"50\" placeholder=\"Institution ID\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required><div class='form-submit-button-container'><button>Login</button>	</div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<a href=\"./contact\"><u>Contact us!</u></a></p></div></div><div class=\"student-login-modal-mobile hidden\"><div id='form-container' class=\"login-form-container student-login-form-container\"><h2>Student Login</h2><form><input data-type=\"text\" autocomplete=\"email\" class=\"form-full-width\" name=\"email\" maxlength=\"50\" placeholder=\"E-mail\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required> 	<div class='form-submit-button-container'><button>Login</button>	</div></form><p>Don't have an account?&nbsp;&nbsp;&nbsp;<u>Sign up here!</u></p></div></div><div class=\"student-register-modal-mobile hidden\"><div id='form-container' class=\"student-register-form-container login-form-container\"><h2>Register</h2><form id='register-form-mobile'><input data-type=\"text\" type=\"e-mail\" class=\"form-full-width\" name=\"email\" maxlength=\"50\" placeholder=\"E-mail\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password\" maxlength=\"50\" placeholder=\"Password\" required><input type=\"password\" autocomplete=\"current-password\" class=\"form-full-width\" name=\"password2\" maxlength=\"50\" placeholder=\"Confirm Password\" required><div class='form-submit-button-container'><button >Register</button></div></form><p>Already have an account?&nbsp;&nbsp;&nbsp;<a href="https://apply.vectoracademy.io"><u>Sign in here!</u></a></p></div></div>`;
 	document.body.appendChild(mainInactive);
 	const footer = document.createElement('footer');
 	footer.innerHTML = `<div class='footer-container'><h1><a href=\"./\">Engineering Apprenticeship in the US.</a></h1><div class='footer-contents-container'><div><h3>About</h3><ul><li><a href=\"./privacy\">Privacy Policy</a></li><li><a href=\"./contact\">Contact</a></li></ul></div><div><h3>Social</h3><ul><li><a href=\"https://www.twitter.com/VectorAcad\" target=\"_blank\">Twitter</a></li><li><a href=\"https://www.facebook.com/vectoracademy.io/\" target=\"_blank\">Facebook</a></li><li><a href=\"https://www.reddit.com/r/vectoracademy/\" target=\"_blank\">Reddit</a></li><li><a href=\"https://www.instagram.com/vectoracad/\" target=\"_blank\">Instagram</a></li><li><a href=\"https://www.linkedin.com/company/37268628/\" target=\"_blank\">LinkedIn</a></li></ul></div><div><h3>Resources</h3><ul><li><a href=\"./faqs\">FAQ</a></li><li><a href=\"./howitworks\">How it works</a></li><li class=\"footer-student-register\">Application</li></ul></div></div></div><p></p>`;
 	document.body.appendChild(footer); 
 
 	//toggle mobile login forms
-	const employerOpenBtnMobile = document.getElementById('employer-login-mobile');
-	//const studentOpenBtnMobile = document.getElementById('student-login-mobile');
+	//const employerOpenBtnMobile = document.getElementById('employer-login-mobile');
 	const studentRegisterBtnMobile = document.querySelector('.student-register-mobile');
-	const employerModalMobile = document.querySelector('.employer-modal-mobile');
-	const studentLoginModalMobile = document.querySelector('.student-login-modal-mobile');
+	//const employerModalMobile = document.querySelector('.employer-modal-mobile');
+	//const studentLoginModalMobile = document.querySelector('.student-login-modal-mobile');
 	const studentRegisterModalMobile = document.querySelector('.student-register-modal-mobile');
-	const registerHereMobile = document.querySelector('.student-login-modal-mobile u');
-	const loginHereMobile = document.querySelector('.student-register-modal-mobile u');
+	//const registerHereMobile = document.querySelector('.student-login-modal-mobile u');
+	//const loginHereMobile = document.querySelector('.student-register-modal-mobile u');
 	const mainInactiveMenu = document.querySelector('.main-inactive-menu');
 
-	employerOpenBtnMobile.addEventListener('click', openEmployerMobile);
-	//studentOpenBtnMobile.addEventListener('click', openLoginStudentMobile);
+	//employerOpenBtnMobile.addEventListener('click', openEmployerMobile);
 	studentRegisterBtnMobile.addEventListener('click', openRegisterStudentMobile);	
-	registerHereMobile.addEventListener('click', openRegisterStudentMobile);
-	loginHereMobile.addEventListener('click', openLoginStudentMobile);
+	//registerHereMobile.addEventListener('click', openRegisterStudentMobile);
+	//loginHereMobile.addEventListener('click', openLoginStudentMobile);
 
-	function openEmployerMobile (e) {
-		mainInactiveMenu.classList.add('hidden');
-		employerModalMobile.classList.remove('hidden');
-	}
+	// function openEmployerMobile (e) {
+	// 	mainInactiveMenu.classList.add('hidden');
+	// 	employerModalMobile.classList.remove('hidden');
+	// }
 
-	function openLoginStudentMobile (e) {
-		mainInactiveMenu.classList.add('hidden');
-		if (!studentRegisterModalMobile.classList.contains('hidden')) studentRegisterModalMobile.classList.add('hidden');
-		studentLoginModalMobile.classList.remove('hidden');
-	}
+	// function openLoginStudentMobile (e) {
+	// 	mainInactiveMenu.classList.add('hidden');
+	// 	if (!studentRegisterModalMobile.classList.contains('hidden')) studentRegisterModalMobile.classList.add('hidden');
+	// 	studentLoginModalMobile.classList.remove('hidden');
+	// }
 
 	function openRegisterStudentMobile (e) {
 		studentLoginModalMobile.classList.add('hidden');
@@ -53,58 +51,58 @@ export default function templatePage () {
 	}
 
 	//handle mobile login requests
-	const employerLoginBtn = document.querySelector(".employer-modal-mobile button");
-	const studentLoginBtn = document.querySelector(".student-login-modal-mobile button");
+	//const employerLoginBtn = document.querySelector(".employer-modal-mobile button");
+	//const studentLoginBtn = document.querySelector(".student-login-modal-mobile button");
 	const studentRegisterBtn = document.querySelector(".student-register-modal-mobile button");
-	employerLoginBtn.addEventListener('click', handleEmployerLogin);
-	studentLoginBtn.addEventListener('click', handleStudentLogin);
+	//employerLoginBtn.addEventListener('click', handleEmployerLogin);
+	//studentLoginBtn.addEventListener('click', handleStudentLogin);
 	studentRegisterBtn.addEventListener('click', handleStudentRegister);
 
 
-	function tempResponse (userType) {
-		const container = document.querySelector(`.${userType}-modal-mobile .login-form-container`);
-		const element = document.querySelector('.employer-modal-mobile .form-submit-button-container');
-		removeSpinner(element);
-		//remove a warning if already present
-		if (document.querySelector(`.${userType}-modal-mobile .warning`)) {
-			const deleteWarning = document.querySelector(`.${userType}-modal-mobile .login-form-container .warning`);
-			container.removeChild(deleteWarning);
-		}		
-		const h2 = document.querySelector(`.${userType}-modal-mobile .login-form-container h2`);
-		let warning = document.createElement('p');
-		warning.classList=('warning');
-		warning.innerHTML = 'No such user exists';
-		container.insertBefore(warning, h2);
-	}
+	// function tempResponse (userType) {
+	// 	const container = document.querySelector(`.${userType}-modal-mobile .login-form-container`);
+	// 	const element = document.querySelector('.employer-modal-mobile .form-submit-button-container');
+	// 	removeSpinner(element);
+	// 	//remove a warning if already present
+	// 	if (document.querySelector(`.${userType}-modal-mobile .warning`)) {
+	// 		const deleteWarning = document.querySelector(`.${userType}-modal-mobile .login-form-container .warning`);
+	// 		container.removeChild(deleteWarning);
+	// 	}		
+	// 	const h2 = document.querySelector(`.${userType}-modal-mobile .login-form-container h2`);
+	// 	let warning = document.createElement('p');
+	// 	warning.classList=('warning');
+	// 	warning.innerHTML = 'No such user exists';
+	// 	container.insertBefore(warning, h2);
+	// }
 
-	function handleEmployerLogin (e) {
-		e.preventDefault();
-		//form Validation
-		var institutionID = document.querySelector(".employer-modal-mobile form").elements["institutionID"].value
-		var password = document.querySelector(".employer-modal-mobile form").elements["password"].value;
+	// function handleEmployerLogin (e) {
+	// 	e.preventDefault();
+	// 	//form Validation
+	// 	var institutionID = document.querySelector(".employer-modal-mobile form").elements["institutionID"].value
+	// 	var password = document.querySelector(".employer-modal-mobile form").elements["password"].value;
 
-	  if (!institutionID || !password) {
-	    return;
-	  } else {
-	  	const element = document.querySelector('.employer-modal-mobile .form-submit-button-container');
-	  	addSpinner(element);
-	  	setTimeout(tempResponse, 800, 'employer');
-	  }	
-	}
+	//   if (!institutionID || !password) {
+	//     return;
+	//   } else {
+	//   	const element = document.querySelector('.employer-modal-mobile .form-submit-button-container');
+	//   	addSpinner(element);
+	//   	setTimeout(tempResponse, 800, 'employer');
+	//   }	
+	// }
 
-	function handleStudentLogin(e) {
-		e.preventDefault();
-		//form Validation
-		const email = document.querySelector(".student-login-modal-mobile form").elements["email"].value
-		const password = document.querySelector(".student-login-modal-mobile form").elements["password"].value;
+	// function handleStudentLogin(e) {
+	// 	e.preventDefault();
+	// 	//form Validation
+	// 	const email = document.querySelector(".student-login-modal-mobile form").elements["email"].value
+	// 	const password = document.querySelector(".student-login-modal-mobile form").elements["password"].value;
 		
-		const data = {
-			email,
-			password
-		}
-			const spinnerElement = document.querySelector('.student-login-modal-mobile form div');
-			submitAuthForm(data, 'studentlogin', spinnerElement);
-	}	
+	// 	const data = {
+	// 		email,
+	// 		password
+	// 	}
+	// 		const spinnerElement = document.querySelector('.student-login-modal-mobile form div');
+	// 		submitAuthForm(data, 'studentlogin', spinnerElement);
+	// }	
 	
 
 	function handleStudentRegister(e) {
@@ -136,9 +134,9 @@ export default function templatePage () {
 	  mainActive.classList.toggle('hidden');
 	  mainInactive.classList.toggle('hidden');
 	  mainInactiveMenu.classList.remove('hidden');
-	  employerModalMobile.classList.add('hidden');
+	 // employerModalMobile.classList.add('hidden');
 	  studentRegisterModalMobile.classList.add('hidden');
-	  studentLoginModalMobile.classList.add('hidden');
+	  //studentLoginModalMobile.classList.add('hidden');
 	  footer.classList.toggle('hidden');
 	}); 
 	
--- marketing-app/src/public/js/header-modals.js
+++ marketing-app/src/public/js/header-modals.js
@@ -2,58 +2,50 @@ import {addSpinner, removeSpinner} from './spinner';
 import submitAuthForm from './submit-auth-form.js';
 
 export default function loadModals() {
-	const employerOpenBtn = document.getElementById('employer-login');
+	//const employerOpenBtn = document.getElementById('employer-login');
 	//const studentOpenBtn = document.getElementById('student-login');
-	const employerModal = document.querySelector('.employer-modal');
+	//const employerModal = document.querySelector('.employer-modal');
 	const studentLoginModal = document.querySelector('.student-login-modal');
 	const studentRegisterModal = document.querySelector('.student-register-modal');
 	const studentApplyButton = document.querySelector('#navbar-container button');
-	const loginHere = document.querySelector('.student-register-modal u');
-	const registerHere = document.querySelector('.student-login-modal u');
+	//const loginHere = document.querySelector('.student-register-modal u');
+	//const registerHere = document.querySelector('.student-login-modal u');
 
-	employerOpenBtn.addEventListener('click', openEmployer);
-	//studentOpenBtn.addEventListener('click', openLoginStudent);
-	employerModal.addEventListener('click', handleCloseEmployer);
-	studentLoginModal.addEventListener('click', handleCloseStudentLogin);
+	//employerOpenBtn.addEventListener('click', openEmployer);
+	//employerModal.addEventListener('click', handleCloseEmployer);
+	//studentLoginModal.addEventListener('click', handleCloseStudentLogin);
 	studentRegisterModal.addEventListener('click', handleCloseStudentRegister);
 	studentApplyButton.addEventListener('click', openApplyStudent)
-	registerHere.addEventListener('click', openRegisterStudent);
-	loginHere.addEventListener('click', openLoginStudent);
+	//registerHere.addEventListener('click', openRegisterStudent);
 
-	function openEmployer (e) {
-		if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
-		if (!studentRegisterModal.classList.contains('hidden')) studentRegisterModal.classList.add('hidden');
-		employerModal.classList.remove('hidden');
-	}
-
-	function openLoginStudent (e) {
-		if (!employerModal.classList.contains('hidden')) employerModal.classList.add('hidden');
-		if (!studentRegisterModal.classList.contains('hidden')) studentRegisterModal.classList.add('hidden');
-		studentLoginModal.classList.remove('hidden');
-	}
+	// function openEmployer (e) {
+	// 	if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
+	// 	if (!studentRegisterModal.classList.contains('hidden')) studentRegisterModal.classList.add('hidden');
+	// 	employerModal.classList.remove('hidden');
+	// }
 
 	function openApplyStudent (e) {
-		if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
-		if (!employerModal.classList.contains('hidden')) employerModal.classList.add('hidden');
+		//if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
+		//if (!employerModal.classList.contains('hidden')) employerModal.classList.add('hidden');
 		studentRegisterModal.classList.remove('hidden');
 	}
 
-	function openRegisterStudent (e) {
-		if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
-		studentRegisterModal.classList.remove('hidden');
-	}
+	// function openRegisterStudent (e) {
+	// 	//if (!studentLoginModal.classList.contains('hidden')) studentLoginModal.classList.add('hidden');
+	// 	studentRegisterModal.classList.remove('hidden');
+	// }
 
-	function handleCloseEmployer (e) {
-		if (e.target.nodeName === 'SPAN' || e.target.classList.contains('employer-modal')) {
-			employerModal.classList.add('hidden');
-		}
-	}
+	// function handleCloseEmployer (e) {
+	// 	if (e.target.nodeName === 'SPAN' || e.target.classList.contains('employer-modal')) {
+	// 		employerModal.classList.add('hidden');
+	// 	}
+	// }
 
-	function handleCloseStudentLogin (e) {
-		if (e.target.nodeName === 'SPAN' || e.target.classList.contains('student-login-modal')) {
-			studentLoginModal.classList.add('hidden');
-		}
-	}
+	// function handleCloseStudentLogin (e) {
+	// 	if (e.target.nodeName === 'SPAN' || e.target.classList.contains('student-login-modal')) {
+	// 		studentLoginModal.classList.add('hidden');
+	// 	}
+	// }
 
 	function handleCloseStudentRegister (e) {
 		if (e.target.nodeName === 'SPAN' || e.target.classList.contains('student-register-modal')) {
@@ -62,11 +54,11 @@ export default function loadModals() {
 	}
 
 	//respond to login requests
-	const employerLoginBtn = document.querySelector(".employer-modal button");
-	const studentLoginBtn = document.querySelector(".student-login-modal button");
+	//const employerLoginBtn = document.querySelector(".employer-modal button");
+	//const studentLoginBtn = document.querySelector(".student-login-modal button");
 	const studentRegisterBtn = document.querySelector(".student-register-modal button");
-	employerLoginBtn.addEventListener('click', handleEmployerLogin);
-	studentLoginBtn.addEventListener('click', handleStudentLogin);
+	// employerLoginBtn.addEventListener('click', handleEmployerLogin);
+	// studentLoginBtn.addEventListener('click', handleStudentLogin);
 	studentRegisterBtn.addEventListener('click', handleStudentRegister);
 
 	function tempResponse (userType) {
@@ -85,35 +77,35 @@ export default function loadModals() {
 		container.insertBefore(warning, h2);
 	}
 
-	function handleEmployerLogin (e) {
-		e.preventDefault();
-		//form Validation
-		const institutionID = document.querySelector(".employer-modal form").elements["institutionID"].value
-		const password = document.querySelector(".employer-modal form").elements["password"].value;
+	// function handleEmployerLogin (e) {
+	// 	e.preventDefault();
+	// 	//form Validation
+	// 	const institutionID = document.querySelector(".employer-modal form").elements["institutionID"].value
+	// 	const password = document.querySelector(".employer-modal form").elements["password"].value;
 
-	  if (!institutionID || !password) {
-	    return;
-	  } else {
-	  	const element = document.querySelector('.employer-modal .form-submit-button-container');
-	  	addSpinner(element);
-	  	setTimeout(tempResponse, 800, 'employer');
-	  }	
-	}
+	//   if (!institutionID || !password) {
+	//     return;
+	//   } else {
+	//   	const element = document.querySelector('.employer-modal .form-submit-button-container');
+	//   	addSpinner(element);
+	//   	setTimeout(tempResponse, 800, 'employer');
+	//   }	
+	// }
 
-	function handleStudentLogin(e) {
-		e.preventDefault();
-		//form Validation
-		const email = document.querySelector(".student-login-modal form").elements["email"].value
-		const password = document.querySelector(".student-login-modal form").elements["password"].value;
+	// function handleStudentLogin(e) {
+	// 	e.preventDefault();
+	// 	//form Validation
+	// 	const email = document.querySelector(".student-login-modal form").elements["email"].value
+	// 	const password = document.querySelector(".student-login-modal form").elements["password"].value;
 		
-		const data = {
-			email,
-			password
-		}
+	// 	const data = {
+	// 		email,
+	// 		password
+	// 	}
 
-			const spinnerElement = document.querySelector('.student-login-modal form div');
-			submitAuthForm(data, 'studentlogin', spinnerElement)
-	}	
+	// 		const spinnerElement = document.querySelector('.student-login-modal form div');
+	// 		submitAuthForm(data, 'studentlogin', spinnerElement)
+	// }	
 
 	function handleStudentRegister(e) {
 		e.preventDefault();
